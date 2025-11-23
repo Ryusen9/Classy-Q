76c6c61daf44fd93e80374d80a0235f3a31539b9
Ryusen9 <asratul02@gmail.com>
2025-11-23 21:33:32 +0600

    Add initial components and styles, integrate Clerk for authentication
    - Created Navbar and Footer components
    - Implemented NavHeader with sign-up promotion
    - Added button component with variants
    - Introduced global styles in globals.css
    - Configured Clerk middleware for authentication
    - Added favicon and logo images
    - Updated package.json and package-lock.json with new dependencies

---
+++ components/NavHeader.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { XIcon } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+const NavHeader = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+  const handleHideFeature = () => {
+    setIsVisible(false);
+  };
+  return (
+    <div
+      className={`w-full bg-black relative text-white text-xs p-0.5 flex items-center justify-center gap-2 ${
+        isVisible ? "flex" : "hidden"
+      }`}
+      onClick={handleHideFeature}
+    >
+      <p>
+        <Link href={"/sign-up"}>
+          <span className="font-semibold underline">Sign Up</span>
+        </Link>
+        and enjoy 10% off your first purchase!
+      </p>
+      <div className="absolute right-10 cursor-pointer">
+        <XIcon size={15} />
+      </div>
+    </div>
+  );
+};
+export default NavHeader;
---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import NavHeader from "./NavHeader";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full z-50 bg-white">
+      <NavHeader />
+    </div>
+  );
+};
+export default Navbar;
